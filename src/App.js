58918ff9c066b694017711f49a548c49f466a5b9
Morne Marais <morne.dev114@gmail.com>
2021-07-14 01:40:07 +0200

    fix: Build 1.1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-import { ChatEngine } from 'react-chat-engine'; 
+import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
-const 
+const projectID = 'd462a826-d3b9-4e14-82b5-6a1b3ddcc9bf';
 
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
